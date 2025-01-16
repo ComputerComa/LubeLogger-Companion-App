6b237a19816f254e257329d4f5d2ec1da98e4d04
ComputerComa <walkerraydick2@gmail.com>
2025-01-16 11:56:37 -0600

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LubeLogger_Companion_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
